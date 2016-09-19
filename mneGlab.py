efbeb98eee49bf3b0f07898e429a600ea7456ad8
Immergo Dev <dirtyolddwarf@gmail.com>
2016-09-19 11:24:52 +0200

    merge changes made by Pawel

--- mneGlab.py
+++ mneGlab.py
@@ -1,7 +1,5 @@
 import mne, csv
 
-montageFileDir = 'd:/home/mskorko/apps/Anaconda2/Lib/site-packages/mne/channels/data/montages/M1_Custom.txt'
-
 def correctFilter(behDataPath):
     """Input path and filename of extracted behavioral responses.  Returns boolean array
        to be used when dropping epochs."""
@@ -18,7 +16,7 @@ def correctFilter(behDataPath):
     return accuracies
 
 
-def loadData(eegDataPath):
+def loadRawData(eegDataPath):
     """Input path and filename to .cnt, return raw data object."""
 
     raw = mne.io.read_raw_cnt(eegDataPath, None, date_format='dd/mm/yy', preload=True)
@@ -30,7 +28,7 @@ def rawPrep(rawObj):
 
     rawObj.rename_channels(mapping={'FP2': 'Fp2', 'As REF': 'AsREF', 'Af7': 'AF7'})
     rawObj.info['bads'] = ['AsREF']
-    montages = mne.channels.read_montage(montageFileDir)
+    montages = mne.channels.read_montage('C:\Users\GamesLab_2\mne-python\mne\channels\data\montages\M1_Custom.txt')
     rawObj.set_montage(montages)
     mne.io.set_eeg_reference(rawObj, ref_channels=None)
 
@@ -55,7 +53,79 @@ def epochRaw(rawObj):
 
     return epochs
 
-#epochs.plot(n_channels, n_epochs) | ie. 32, 15
-#epochs.save(path/filename-epo.fif)
+
+def loadEpochs(directory):
+    """Input directory of .fif epoch files.  Return list of epoch objects and list of epoch filenames."""
+
+    fileList = []
+    for file in directory:
+        if file.endswith('.fif'):
+            fileList.append(file)
+    print "File Names Gathered: ", fileList
+
+    epochList = []
+    for ep in fileList:
+        epoch = mne.read_epochs(ep)
+        epochList.append(epoch)
+
+    return epochList, fileList
+
+
+def loadICA(directory):
+    """Input directory of ICA weight .fif files.  Return list of ICA objects."""
+
+    icaList = []
+    for file in directory:
+        if file.endswith('ica.fif'):
+            ica = mne.preprocessing.read_ica(file)
+            icaList.append(ica)
+
+    return icaList
+
+
+def getICA(epochList, fileList):
+    """Input list of epoch objects.  Save ICA solutions for each participant."""
+
+    for num, obj in enumerate(epochList):
+        this_ica = mne.preprocessing.ICA(method='extended-infomax').fit(obj)
+        this_file = fileList[num]
+        fileName = this_file[:this_file.find('_')] + "VWM_solution-ica.fif"
+        this_ica.save(fileName)
+
+
+def epochForICA(directory):
+    """Input directory of raw .cnt.  Prepare, filter, and save."""
+
+    for file in directory:
+        if file.endswith('.cnt'):
+            print "Current file: " + str(file)
+            raw = loadRawData(file)
+            print raw.info
+            rawPrep(raw)
+            rawResample_Filter(raw)
+
+            epochs = epochRaw(raw)
+            epochs.save(file[:file.find('.')] + "_forICA-epo.fif", split_size='2GB')
+
+
+def epochCorrectData(directory):
+    """Input directory of raw .cnt.  Drop incorrect trial epochs and save."""
+
+    for file in directory:
+        if file.endswith('.cnt'):
+            print "Current file: " + str(file)
+            raw = loadRawData(file)
+            rawPrep(raw)
+            rawResample_Filter(raw)
+
+            epochs = epochRaw(raw)
+            accuracies = correctFilter(file[:file.find('_')]+"_markers.csv")
+
+            print "Number of epochs: " + str(len(epochs.events))
+            print "Number of behavioral responses" + str(len(accuracies))
+
+            epochs.drop(accuracies, reason='USER')
+            epochs.save(file[:file.find('.')]+"_correctFiltered-epo.fif", split_size='2GB')
+
 #epochs.drop(accuracies, reason='USER')
 #dataPath = 'C:/Users/GamesLab_2/Desktop/Py EEG Analysis/'
